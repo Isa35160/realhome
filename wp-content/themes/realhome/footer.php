4f8e9a2a478efc39a3a2ae862519643dee3738ce
Isa35160 <teixeirarisabel@gmail.com>
2019-01-20 20:24:49 +0100

    ajout section points forts

--- wp-content/themes/realhome/footer.php
+++ wp-content/themes/realhome/footer.php
@@ -1,26 +1,40 @@
 <?php wp_footer(); ?>
 
 <footer>
-    <div class="container">
-        <div class="footer-nav">
-            <div class="footer-logo">
-                <a class="logo" href="/realhome"><span class="real">REAL</span><span class="home">HOME</span></a>
-                <div class="footer_logo_border footer-top-left"></div>
-                <div class="footer_logo_border footer-top-right"></div>
-                <div class="footer_logo_border footer-bottom-left"></div>
-                <div class="footer_logo_border footer-bottom-right"></div>
-            </div>
+          <div class="container">
+                    <div class="footer-nav">
+                              <div class="footer-logo-rs">
+                                        <div class="footer-logo">
+                                                  <a class="logo" href="/realhome"><span class="real">REAL</span><span
+                                                                      class="home">HOME</span></a>
+                                                  <div class="footer_logo_border footer-top-left"></div>
+                                                  <div class="footer_logo_border footer-top-right"></div>
+                                                  <div class="footer_logo_border footer-bottom-left"></div>
+                                                  <div class="footer_logo_border footer-bottom-right"></div>
+                                        </div>
+                                        <div class="footer_nav_rs">
+                                            <?php wp_nav_menu(array('theme_location' => 'menu-secondaire', 'container' => 'nav')); ?>
+                                        </div>
+                              </div>
+                              <div class="footer_nav_principal">
+                                        <h3 class="footer_title">Menu</h3>
+                                  <?php wp_nav_menu(array('theme_location' => 'menu-principal', 'container' => 'nav')); ?>
+                              </div>
+                              <div></div>
+                              <div></div>
+                              <div class="footer_contact">
+                                        <h3 class="footer_title">Contact</h3>
+                                        <p><?php the_field('adresse', 'option'); ?></p>
+                                        <p>Freephone : <?php the_field('freephone', 'option'); ?></p>
+                                        <p>Telephone : <?php the_field('telephone', 'option'); ?></p>
+                                        <p>Fax : <?php the_field('fax', 'option'); ?></p>
+                                        <p><a href="mailto:<?php the_field('email', 'option'); ?>"><?php the_field('email', 'option'); ?></a></p>
 
-                  <div class="nav_rs">
-                      <?php wp_nav_menu(array('theme_location' => 'menu-secondaire', 'container' => 'nav')); ?>
-                  </div>
-            <div class="nav_principal">
-                <?php wp_nav_menu(array('theme_location' => 'menu-principal', 'container' => 'nav')); ?>
-            </div>
 
-        </div>
-    </div>
+                              </div>
 
+                    </div>
+          </div>
 
 
 </footer>
--- wp-content/themes/realhome/front-page.php
+++ wp-content/themes/realhome/front-page.php
@@ -12,80 +12,171 @@
     <?php
     $field = get_field_object('notre_agence_au_plus_pres_de_vous');
     ?>
-          <h2 class="notre_agence_title"><?php echo($field['label']); ?></h2>
-          <div class="border-bottom-right"></div>
+          <div class="notre_agence_bloc_title">
+                    <h2 class="notre_agence_title"><?php echo($field['label']); ?></h2>
+                    <div class="notre_agence_underline"></div>
+          </div>
           <p class="notre_agence_desc"><?php the_field('notre_agence_au_plus_pres_de_vous'); ?></p>
 </div>
 
+<!--     NOS POINTS FORTS     -->
 
-<!--     NOS PROPRIETES     -->
-<div class="container nos-proprietes">
-          <h2>Nos Propriétés</h2>
-          <p>Quisque diam lorem interdum vitaapibus vitae pede. Donec eget tellus non
-                    erat lacinia fertum. Donec in velit vel ipsum auctovinar.</p>
-          <div class="proprietes_cards">
-              <?php
+<div class="points_forts ">
+          <div class="container">
+              <?php if (have_rows('points_forts')): ?>
 
-              $args = array(
-                  'post_type' => 'proprietes',
-                  'post_per_page' => 6,
-                  'order' => 'ASC',
-              );
-              // The Query
-              $the_query = new WP_Query($args);
+                        <ul class="points_forts_vignette">
 
+                            <?php while (have_rows('points_forts')): the_row();
 
-              if ($the_query->have_posts()) : ?>
-                  <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-                      <?php $proprietes = get_terms('proprietes', array(
-                          'hide_empty' => false,
-                      ));
+                                // vars
+                                $image = get_sub_field('image');
+                                $title = get_sub_field('titre');
+                                $description = get_sub_field('description');
 
-                      ?>
+                                ?>
 
-                                  <div class="card">
-                                            <div class="thumbnail"><img class="thumbnail_image"
-                                                                        src="<?php the_post_thumbnail_url('medium-large') ?> "
-                                                                        alt=""></div>
-                                            <div class="propriete_title">
-                                                      <a href="<?php the_permalink() ?>">
+                                      <li class="">
+                                                <div class="points_forts_img"><img src="<?php echo $image['url']; ?>"
+                                                                                   alt="<?php echo $image['alt'] ?>"/>
+                                                </div>
+                                                <h3 class="points_forts_title"><?php echo $title; ?></h3>
 
-                                                          <?php the_title() ?> -
+                                                <div class="points_forts_description"><?php echo $description; ?></div>
 
-                                                          <?php $id = get_the_ID(); ?>
-                                            </div>
-                                            </a>
-                                            <div class="propriete_city"> <?php $term_list = wp_get_post_terms($post->ID, 'villes', array("fields" => "all"));
-                                                echo $term_list[0]->name; ?></div>
-                                            <div class="propriete_price">
-                                                <?php $price = get_field_object('prix'); ?>
-                                                <?php echo number_format(get_field('prix'), 0, ',', ' '); ?>
-                                                <?php echo($price['append']); ?>
-                                            </div>
-                                            <div class="propriete_infos">
+                                      </li>
 
-                                                <?php $size = get_field_object('surface'); ?>
-                                                <?php $rooms = get_field_object('chambres'); ?>
-                                                <?php $bathrooms = get_field_object('salle_de_bain'); ?>
+                            <?php endwhile; ?>
 
-
-                                                      <p class="surface"> <?php the_field('surface'); ?>
-                                                          <?php echo($size['append']); ?></p>
-                                                      <p class="chambres"> <?php the_field('chambres'); ?>
-                                                          <?php echo($rooms['append']); ?></p>
-                                                      <p class="sbd"> <?php the_field('salle_de_bain'); ?>
-                                                          <?php echo($bathrooms['append']); ?></p>
-                                            </div>
-                                  </div>
-
-
-                  <?php endwhile; ?>
-                  <?php ?>
+                        </ul>
 
               <?php endif; ?>
           </div>
 
 </div>
 
+
+<!--     NOS PROPRIETES     -->
+<div class="nos-proprietes">
+          <div class="container">
+                    <h2 class="nos_proprietes_title"><span>Nos</span> <span style="font-weight: bold">Propriétés</span>
+                    </h2>
+                    <div class="nos_proprietes_underline"></div>
+                    <p class="nos_proprietes_desc">Quisque diam lorem interdum vitaapibus vitae pede. Donec eget tellus
+                              non
+                              erat lacinia fertum. Donec in velit vel ipsum auctovinar.</p>
+                    <div class="proprietes_cards">
+                        <?php
+
+                        $args = array(
+                            'post_type' => 'proprietes',
+                            'post_per_page' => 6,
+                            'order' => 'ASC',
+                        );
+                        // The Query
+                        $the_query = new WP_Query($args);
+
+
+                        if ($the_query->have_posts()) : ?>
+                            <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                                <?php $proprietes = get_terms('proprietes', array(
+                                    'hide_empty' => false,
+                                ));
+
+                                ?>
+
+                                            <div class="card">
+                                                      <div class="thumbnail"><img class="thumbnail_image"
+                                                                                  src="<?php the_post_thumbnail_url('medium-large') ?> "
+                                                                                  alt=""></div>
+                                                      <div class="propriete_title">
+                                                                <a href="<?php the_permalink() ?>">
+
+                                                                    <?php the_title() ?> -
+
+                                                                    <?php $id = get_the_ID(); ?>
+                                                      </div>
+                                                      </a>
+                                                      <div class="propriete_city"> <?php $term_list = wp_get_post_terms($post->ID, 'villes', array("fields" => "all"));
+                                                          echo $term_list[0]->name; ?></div>
+                                                      <div class="propriete_price">
+                                                          <?php $price = get_field_object('prix'); ?>
+                                                          <?php echo number_format(get_field('prix'), 0, ',', ' '); ?>
+                                                          <?php echo($price['append']); ?>
+                                                      </div>
+                                                      <div class="propriete_infos">
+
+                                                          <?php $size = get_field_object('surface'); ?>
+                                                          <?php $rooms = get_field_object('chambres'); ?>
+                                                          <?php $bathrooms = get_field_object('salle_de_bain'); ?>
+
+
+                                                                <p class="surface"> <?php the_field('surface'); ?>
+                                                                    <?php echo($size['append']); ?></p>
+                                                                <p class="chambres"> <?php the_field('chambres'); ?>
+                                                                    <?php echo($rooms['append']); ?></p>
+                                                                <p class="sbd"> <?php the_field('salle_de_bain'); ?>
+                                                                    <?php echo($bathrooms['append']); ?></p>
+                                                      </div>
+                                            </div>
+
+
+                            <?php endwhile; ?>
+                            <?php ?>
+
+                        <?php endif; ?>
+                    </div>
+          </div>
+
+<!--     NOS PARTENAIRES     -->
+
+          <h2>Our Partners</h2>
+    <?php
+
+    $images = get_field('our_partners');
+    $size = 'full'; // (thumbnail, medium, large, full or custom size)
+
+    if ($images): ?>
+              <ul>
+                  <?php foreach ($images as $image): ?>
+                            <li>
+                                <?php echo wp_get_attachment_image($image['ID'], $size); ?>
+                            </li>
+                  <?php endforeach; ?>
+              </ul>
+    <?php endif; ?>
+
+</div>
+
+<!--     NOS AGENTS     -->
+<h2>Nos Agents</h2>
+
+<?php if (have_rows('nos_agents')): ?>
+
+          <ul class="nos_agents">
+
+              <?php while (have_rows('nos_agents')): the_row();
+
+                  // vars
+                  $image = get_sub_field('image');
+                  $name = get_sub_field('nom_prenom');
+                  $presentation = get_sub_field('presentation');
+
+                  ?>
+
+                        <li class="">
+                                  <div class="nos_agents_img"><img src="<?php echo $image['url']; ?>"
+                                                                     alt="<?php echo $image['alt'] ?>"/>
+                                  </div>
+                                  <h3 class="nos_agents_name"><?php echo $name; ?></h3>
+
+                                  <div class="nos_agents_presentation"><?php echo $presentation; ?></div>
+                        </li>
+
+              <?php endwhile; ?>
+
+          </ul>
+
+<?php endif; ?>
 
 <?php get_footer(); ?>
